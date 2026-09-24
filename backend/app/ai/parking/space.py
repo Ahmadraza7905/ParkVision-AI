ec7d003ef535ec2d1af907c61b3930c451dbf2aa
Ahmad Raza <jiyakhan7905@gmail.com>
2026-09-24 14:57:33 +0530

    Add parking space model

---
+++ backend/app/ai/parking/space.py
@@ -0,0 +1,10 @@
+﻿from dataclasses import dataclass
+from typing import Tuple
+
+
+@dataclass
+class ParkingSpace:
+    """Represents a parking space in an image."""
+
+    id: str
+    polygon: Tuple[Tuple[float, float], ...]
